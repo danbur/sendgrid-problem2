848b098d21412f50f4dd3be34a248e2e1b4213fe
Daniel Burton <danielburton5@gmail.com>
2013-12-09 09:55:55 -0800

    First attempt to get form entry working

--- tests/api_workshop_page.py
+++ tests/api_workshop_page.py
@@ -2,25 +2,69 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+TIMEOUT = 30
 IFRAME_BY = By.TAG_NAME
 IFRAME_LOCATOR = "iframe"
 CREDENTIALS_BY = By.ID
 CREDENTIALS_LOCATOR = "credentials"
 LOGIN_BY = By.XPATH
-LOGIN_LOCATOR = "//*[@id='credentials']//*[label[text()='Username']]/input"
+LOGIN_LOCATOR = "//form[@id='credentials']//div[label[text()='Username']]"\
+    "/input"
 PASSWORD_BY = By.XPATH
-PASSWORD_LOCATOR = "//*[@id='credentials']//*[label[text()='Password']]""/input"
+PASSWORD_LOCATOR = "//form[@id='credentials']//div[label[text()='Password']]"\
+    "/input"
+SECTIONS_BY = By.CSS_SELECTOR
+SECTIONS_LOCATOR = "li.endpoint"
+SECTION_NAME_BY = By.CSS_SELECTOR
+SECTION_NAME_LOCATOR = ".title .name"
+FORM_EXPANDER_BY = By.CSS_SELECTOR
+FORM_EXPANDER_LOCATOR = ".expand-methods"
+FORM_BY = By.TAG_NAME
+FORM_LOCATOR = "form"
+TO_EMAIL_BY = By.NAME
+TO_EMAIL_LOCATOR = "params[to]"
+TO_NAME_BY = By.NAME
+TO_NAME_LOCATOR = "params[toname]"
 
 class ApiWorkshopPage:
     def __init__(self, driver):
+        self._cache_mail_section = None
         self.driver = driver
         driver.switch_to_frame(driver.find_element(IFRAME_BY, IFRAME_LOCATOR))
-        WebDriverWait(driver, 30).until(EC.presence_of_element_located(\
+        WebDriverWait(driver, TIMEOUT).until(\
+            EC.presence_of_element_located(\
                 (CREDENTIALS_BY, CREDENTIALS_LOCATOR)))
+
+    def _mail_section(self):
+        if self._cache_mail_section:
+            return self._cache_mail_section
+
+        sections = self.driver.find_elements(SECTIONS_BY, SECTIONS_LOCATOR)
+        for section in sections:
+            section_name = \
+                section.find_element(SECTION_NAME_BY, SECTION_NAME_LOCATOR)
+            if section_name.text == 'Mail':
+                self._cache_mail_section = section
+                return section
+        raise Exception("Mail section not found")
 
     def enter_login(self, login):
         self.driver.find_element(LOGIN_BY, LOGIN_LOCATOR).send_keys(login)
 
     def enter_password(self, password):
-        self.driver.find_element(PASSWORD_BY, PASSWORD_LOCATOR).\
-            send_keys(password)
+        self.driver.find_element(PASSWORD_BY, PASSWORD_LOCATOR)\
+            .send_keys(password)
+
+    def open_send_mail_api_form(self):
+        self._mail_section().\
+            find_element(FORM_EXPANDER_BY, FORM_EXPANDER_LOCATOR).click()
+        form = self._mail_section().find_element(FORM_BY, FORM_LOCATOR)
+        WebDriverWait(self.driver, TIMEOUT).until(EC.visibility_of(form))
+
+    def enter_to_email(self, email):
+        self._mail_section().find_element(TO_EMAIL_BY, TO_EMAIL_LOCATOR)\
+            .send_keys(name)
+
+    def enter_to_name(self, name):
+        self._mail_section().find_element(TO_NAME_BY, TO_NAME_LOCATOR)\
+            .send_keys(name)
--- tests/features/steps.py
+++ tests/features/steps.py
@@ -1,26 +1,29 @@
 from lettuce import *
 from api_workshop_page import ApiWorkshopPage
-import credentials
+import settings
 
-@step(u'Given I am on the SendGrid API Workshop page')
+@step('I am on the SendGrid API Workshop page')
 def on_the_sendgrid_api_workshop_page(step):
     world.driver.get("http://sendgrid.com/docs/api_workshop.html")
     world.page = ApiWorkshopPage(world.driver)
 
-@step(u'When I enter my credentials in the login form')
-def when_i_enter_my_credentials_in_the_login_form(step):
-    world.page.enter_login(credentials.LOGIN)
-    world.page.enter_password(credentials.PASSWORD)
+@step('I enter my credentials in the login form')
+def enter_credentials_in_login_form(step):
+    world.page.enter_login(settings.LOGIN)
+    world.page.enter_password(settings.PASSWORD)
 
-@step(u'When I open the send mail API form')
-def when_i_open_the_send_mail_api_form(step):
-    assert False, 'This step must be implemented'
-@step(u'When I enter valid values in all the fields of the send mail API form')
-def when_i_enter_valid_values_in_all_the_fields_of_the_send_mail_api_form(step):
-    assert False, 'This step must be implemented'
-@step(u'When I click on the Try It button on the send mail API form')
+@step(u'I open the send mail API form')
+def open_send_mail_api_form(step):
+    world.page.open_send_mail_api_form()
+
+@step(u'I enter valid values in all the fields of the send mail API form')
+def enter_values_in_send_mail_api_form(step):
+    world.page.enter_to(settings.EMAIL)
+    world.page.enter_to_name(settings.NAME)
+
+@step(u'I click on the Try It button on the send mail API form')
 def when_i_click_on_the_try_it_button_on_the_send_mail_api_form(step):
     assert False, 'This step must be implemented'
-@step(u'Then the send mail API form should display a "([^"]*)" response')
+@step(u'the send mail API form should display a "([^"]*)" response')
 def then_the_send_mail_api_form_should_display_a_group1_response(step, group1):
     assert False, 'This step must be implemented'
